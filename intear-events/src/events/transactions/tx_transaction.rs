5fca2311107fe91b51b60d245223037685e4d791
Slime <4sliman4@gmail.com>
2024-12-16 04:43:12 +0200

    Fix transaction type

--- intear-events/src/events/transactions/tx_transaction.rs
+++ intear-events/src/events/transactions/tx_transaction.rs
@@ -1,4 +1,4 @@
-use inindexer::near_indexer_primitives::types::{AccountId, BlockHeight};
+use inindexer::near_indexer_primitives::types::{AccountId, Balance, BlockHeight};
 use inindexer::near_indexer_primitives::CryptoHash;
 use inindexer::near_utils::dec_format;
 use schemars::JsonSchema;
@@ -21,13 +21,20 @@ pub struct TxTransactionEvent {
     pub signer_account_id: AccountId,
     #[schemars(with = "String")]
     pub receiver_account_id: AccountId,
-    pub status: String,
+    pub status: &'static str,
     #[schemars(with = "String")]
     pub converted_into_receipt_id: CryptoHash,
     pub receipt_conversion_gas_burnt: u64,
-    pub receipt_conversion_tokens_burnt: u64,
+    #[serde(with = "dec_format")]
+    #[schemars(with = "String")]
+    pub receipt_conversion_tokens_burnt: Balance,
 }
 
 impl TxTransactionEvent {
     pub const ID: &'static str = "tx_transaction";
+
+    pub const STATUS_SUCCESS_VALUE: &'static str = "SUCCESS_VALUE";
+    pub const STATUS_SUCCESS_RECEIPT_ID: &'static str = "SUCCESS_RECEIPT_ID";
+    pub const STATUS_FAILURE: &'static str = "FAILURE";
+    pub const STATUS_UNKNOWN: &'static str = "UNKNOWN";
 }
