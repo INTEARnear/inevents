7d3f7ffdb5c755334e4db893c120cd07f0b3062f
Slime <4sliman4@gmail.com>
2025-01-06 06:27:00 +0200

    Revert transaction type change

--- intear-events/src/events/transactions/tx_transaction.rs
+++ intear-events/src/events/transactions/tx_transaction.rs
@@ -1,4 +1,4 @@
-use inindexer::near_indexer_primitives::types::{AccountId, Balance, BlockHeight};
+use inindexer::near_indexer_primitives::types::{AccountId, BlockHeight};
 use inindexer::near_indexer_primitives::CryptoHash;
 use inindexer::near_utils::dec_format;
 use schemars::JsonSchema;
@@ -11,34 +11,18 @@ pub struct TxTransactionEvent {
     pub block_timestamp_nanosec: u128,
     pub block_height: BlockHeight,
     #[schemars(with = "String")]
-    pub transaction_hash: CryptoHash,
+    pub signer_id: AccountId,
     #[schemars(with = "String")]
-    pub included_in_block_hash: CryptoHash,
+    pub public_key: String,
+    pub nonce: u64,
     #[schemars(with = "String")]
-    pub included_in_chunk_hash: CryptoHash,
-    pub index_in_chunk: u64,
+    pub receiver_id: AccountId,
+    pub priority_fee: Option<u64>,
+    pub signature: String,
     #[schemars(with = "String")]
-    pub signer_account_id: AccountId,
-    #[schemars(with = "String")]
-    pub receiver_account_id: AccountId,
-    pub status: TxTransactionStatus,
-    #[schemars(with = "String")]
-    pub converted_into_receipt_id: CryptoHash,
-    pub receipt_conversion_gas_burnt: u64,
-    #[serde(with = "dec_format")]
-    #[schemars(with = "String")]
-    pub receipt_conversion_tokens_burnt: Balance,
+    pub transaction_id: CryptoHash,
 }
 
 impl TxTransactionEvent {
     pub const ID: &'static str = "tx_transaction";
-}
-
-#[derive(Debug, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
-pub enum TxTransactionStatus {
-    SuccessValue,
-    SuccessReceiptId,
-    Failure,
-    Unknown,
 }
